d92a988ba44afb79fd5c3378450676a732ecb692
richardcharles1502 <50021359+richardcharles1502@users.noreply.github.com>
2019-04-26 20:34:14 +0530

    test
    testing

---
+++ test.php
@@ -0,0 +1,18 @@
+<html>
+	<form name="test2" id ="test" method ="POST">
+<input type ="text" id="test" name="test" value ="tester git hub">
+<input type ="text" id="test1" name="test1" value ="tester git hub and my file ">
+<input type="submit" name="submit" id="submit">
+</form>
+</html>
+
+<?php
+
+$test =  $_POST;
+$test1 = $_POST;
+
+//~ print_r( $test);
+print_r( $test1);
+
+
+?>
